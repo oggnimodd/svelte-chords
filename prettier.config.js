19faa9024b08c67006dc30b63a333d92e1394290
oggnimodd <arnold.ds.dev@gmail.com>
2025-03-10 02:04:48 +0000

    init

---
+++ prettier.config.js
@@ -0,0 +1,22 @@
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+const config = {
+  trailingComma: "es5",
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: false,
+  plugins: ["prettier-plugin-svelte", "prettier-plugin-tailwindcss"],
+  overrides: [
+    {
+      files: "*.svelte",
+      options: {
+        parser: "svelte",
+      },
+    },
+  ],
+};
+
+export default config;
